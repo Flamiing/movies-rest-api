9ddaa799ccb8d4ccd67c55901f3235eefee582d8
Flamiing <amirlaaouam@gmail.com>
2024-02-08 19:28:39 +0100

    Deploy Rest API to Fl0

---
+++ schemas/movies.js
@@ -0,0 +1,35 @@
+const z = require('zod')
+
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: 'Movie title must be a string.',
+    required_error: 'Movie title is requiered.'
+  }),
+  year: z.number().int().min(1900).max(new Date().getFullYear()),
+  director: z.string(), 
+  duration: z.number().int().positive(),
+  poster: z.string().url({
+    message: 'Poster must be a valid URL.'
+  }),
+  genre: z.array(
+    z.enum(['Action', 'Drama', 'Comedy', 'Adventure', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi', 'Crime']),
+    {
+      required_error: 'Movie genre is required.',
+      invalid_type_error: 'Movie genre must be an array of enum.'
+    }
+  ),
+  rate: z.number().min(0).max(10).default(0)
+})
+
+function validateMovie(input) {
+  return movieSchema.safeParse(input)
+}
+
+function validatePartialMovie(input) {
+  return movieSchema.partial().safeParse(input)
+}
+
+module.exports = {
+  validateMovie,
+  validatePartialMovie
+}
