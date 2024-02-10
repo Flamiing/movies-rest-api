fa8890a20f42cb6aecb3e8ca821a24eb88478bea
Flamiing <amirlaaouam@gmail.com>
2024-02-10 06:18:51 +0100

    create added to models

--- models/mySQL/movie.js
+++ models/mySQL/movie.js
@@ -19,14 +19,15 @@ export class MovieModel {
   // Gets movies from database and adds the respective genres
   static async #_getMoviesWithGenres(movies) {
     const moviesPromises = movies.map(async (movie) => {
+      if (!movie) return
       const [genres_id] = await connection.query(
-        'SELECT movie_id, genre_id FROM movie_genres WHERE BIN_TO_UUID(movie_id) = ?',
+        'SELECT movie_id, genre_id FROM movie_genres WHERE BIN_TO_UUID(movie_id) = ?;',
         [movie.id]
       )
         
       const genresPromises = genres_id.map(async (item) => {
         const [[genre]] = await connection.query(
-          'SELECT id, name FROM genre WHERE id = ?',
+          'SELECT id, name FROM genre WHERE id = ?;',
           [item.genre_id]
           )
           return genre.name
@@ -68,20 +69,24 @@ export class MovieModel {
       })
       
       const movies = await Promise.all(moviesPormises)
-      return this.#_getMoviesWithGenres(movies)
+      if (movies.length === 0) return []
+
+      return await this.#_getMoviesWithGenres(movies)
   }
 
   // Gets all movies or filtered by genre
   static async getAll ({ genre }) {
     if (genre) {
-      return this.#_getFilteredByGenre(genre)
+      return await this.#_getFilteredByGenre(genre)
     }
     
     const [movies] = await connection.query(
-      'SELECT BIN_TO_UUID(id) id, title, year, director, duration, poster, rate FROM movie'
+      'SELECT BIN_TO_UUID(id) id, title, year, director, duration, poster, rate FROM movie;'
     )
-      
-    return this.#_getMoviesWithGenres(movies)
+    
+    if (movies.length === 0) return []
+
+    return await this.#_getMoviesWithGenres(movies)
   }
 
   // Gets movie matching given ID
@@ -91,12 +96,48 @@ export class MovieModel {
       [id]
     )
 
-    return this.#_getMoviesWithGenres(movie)
+    if (movie.length === 0) return null
+
+    return await this.#_getMoviesWithGenres(movie)
   }
 
   // Creates new movie into the database
   static async create ({ input }) {
-    
+    const {
+      genre,
+      title,
+      year,
+      director,
+      duration,
+      poster,
+      rate
+    } = input
+
+    const [uuidResult] = await connection.query('SELECT UUID() uuid;')
+    const [{ uuid }] = uuidResult
+
+    try {
+      await connection.query(
+        `INSERT INTO movie (id, title, year, director, duration, poster, rate)
+          VALUES (UUID_TO_BIN(?), ?, ?, ?, ?, ?, ?);`,
+        [uuid, title, year, director, duration, poster, rate]
+      )
+
+      const genresPromises = await genre.map(async (movieGenre) => {
+        await connection.query(
+          `INSERT INTO movie_genres
+            VALUES ((SELECT id FROM movie WHERE title = ? LIMIT 1),
+            (SELECT id FROM genre WHERE name = ? LIMIT 1));`,
+          [title, movieGenre]
+        )
+      })
+      await Promise.all(genresPromises)
+    } catch (e) {
+      console.error('Error creating movie')
+    }
+
+    const movie = await this.getById({ id: uuid })
+    return movie
   }
 
   // Deletes movie matching the given ID
